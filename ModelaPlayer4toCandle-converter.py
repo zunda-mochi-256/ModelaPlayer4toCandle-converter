6d86a5e41e860da46c55ee0778ed1aff06ed8518
Zunda-Mochi <34743727+zunda-mochi-256@users.noreply.github.com>
2023-07-22 02:17:05 +0900

    RapidSpeedSport

--- ModelaPlayer4toCandle-converter.py
+++ ModelaPlayer4toCandle-converter.py
@@ -19,6 +19,10 @@ def Conversion(file_in):
         while True:
             text = f_in.readline()
             if text:
+                result = re.findall(r'^Z1000$', text) #RapidSpeedSupport 
+                if len(result) != 0: #Z1000の上にF1000を追加
+                    text = text.replace(result[0],'F1000\n'+result[0])
+                    print(text)
                 result = re.findall(r'[XYZ]-?\d+', text)
                 if len(result) != 0: #x or y or zがあった
                     for val in result:
@@ -42,7 +46,8 @@ def Conversion(file_in):
 
                 #################################################
                 if re.match(r"\n", text): #改行文字のみの行を検索
-                    print("空白行除去")
+                    # print("空白行除去")
+                    print()
                 else:
                     out_data = out_data+text
                 #################################################
